820e83d5709e22f9dc85d4355bd0bf8a6f3b3b50
Anna S <annasha.gm@gmail.com>
2020-07-22 20:24:03 +0300

    test add

---
+++ TrueTable.java
@@ -0,0 +1,8 @@
+public class TrueTable {
+    public static void main(String[] args) {
+        boolean[] a={false,true,false,true};
+        boolean[] b={false,false,true,true};
+        System.out.println("A\tB\tAND\tOR\tXOR\tNOT");
+        System.out.println(a[0]+a[1]); 
+    }
+}
