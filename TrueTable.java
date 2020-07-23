1c2a6342eea072d4de0c7a1838698c6913caad9f
Anna S <annasha.gm@gmail.com>
2020-07-23 23:22:38 +0300

    +TT

--- TrueTable.java
+++ TrueTable.java
@@ -2,8 +2,8 @@ public class TrueTable {
     public static void main(String[] args) {
         boolean[] a={false,true,false,true};
         boolean[] b={false,false,true,true};
-        System.out.println("A\tB\tAND\tOR\tXOR\tNOT");
+        System.out.println("A\t\tB\t\tAND\t\tOR\t\tXOR\t\tNOT A");
         int c;
-        for(c=0;c<4;c++)System.out.println(a[c]&b[c]); 
+        for(c=0;c<4;c++)System.out.println(a[c]+"\t"+b[c]+"\t"+(a[c]&b[c])+"\t"+(a[c]|b[c])+"\t"+(a[c]^b[c])+"\t"+(!a[c])); 
     }
 }
